0619a6bdf5b536f2b664d2474767fabba60a014a
Konstantin Bazhenov <kot.bazhenov@ya.ru>
2023-12-18 21:03:01 +0300

    bugfix

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,33 +1,19 @@
-const dropdownList = document.querySelector('.dropdown__list');
-const dropdownButton = document.querySelector('.dropdown__value');
-const dropdownListItems = Array.from(document.querySelectorAll('.dropdown__item'));
+const dropdownList = Array.from(document.querySelectorAll('.dropdown__list'));
+const dropdownButton = Array.from(document.querySelectorAll('.dropdown__value'));
+const dropdownListItems = Array.from(document.querySelectorAll('.dropdown__link'));
 
-dropdownButton.addEventListener('click', () => {
-		dropdownList.classList.toggle('dropdown__list_active');
+dropdownButton.forEach((item, i) => {
+	item.addEventListener('click', () => {
+		dropdownList[i].classList.toggle('dropdown__list_active');
+
+		dropdownListItems.forEach((element, j) => {
+		    element.addEventListener('click', (event) => {
+		    	event.preventDefault();
+			    element.closest('.dropdown__list').classList.remove('dropdown__list_active');
+			    element.closest('.dropdown__list').previousElementSibling.textContent = element.textContent;
+			});  
+		});
+
+	});	
+
 });
-
-dropdownListItems.forEach((item, i) => {
-    item.onclick = () => {
-	    dropdownList.classList.remove('dropdown__list_active');
-	    dropdownButton.textContent = dropdownListItems[i].textContent;
-	    return false;
-    };
-});
-
-// const dropdownList = Array.from(document.querySelectorAll('.dropdown__list'));
-// const dropdownButton = Array.from(document.querySelectorAll('.dropdown__value'));
-// const dropdownListItems = Array.from(document.querySelectorAll('.dropdown__item'));
-
-// dropdownButton.forEach((item, i) => {
-// 	item.addEventListener('click', () => {
-// 		dropdownList[i].classList.toggle('dropdown__list_active');
-// 	});	
-// });
-
-// dropdownListItems.forEach((item, i) => {
-//     item.onclick = () => {
-// 	    item.closest('.dropdown__list').classList.remove('dropdown__list_active');
-// 	    item.closest('.dropdown__value').textContent = dropdownListItems[i].textContent;
-// 	    return false;
-//     };
-// });
